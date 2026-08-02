0ed2c934cb63fca9993ba6290eac62c67dcba257
Shan <saan_sfs1@outlook.com>
2026-08-02 17:39:40 +0530

    fixed code bug

--- environments/dev/eks/main.tf
+++ environments/dev/eks/main.tf
@@ -2,10 +2,26 @@ module "eks" {
 
   source = "../../../modules/eks"
 
-  cluster_name    = var.cluster_name
-  cluster_version = var.cluster_version
+  cluster_name       = var.cluster_name
+  cluster_version    = var.cluster_version
 
   vpc_id             = var.vpc_id
   private_subnet_ids = var.private_subnet_ids
 
+  instance_types = var.instance_types
+
+  desired_size = var.desired_size
+  min_size     = var.min_size
+  max_size     = var.max_size
+
+  disk_size = var.disk_size
+
+  capacity_type = var.capacity_type
+
+  endpoint_public_access  = var.endpoint_public_access
+  endpoint_private_access = var.endpoint_private_access
+
+  public_access_cidrs = var.public_access_cidrs
+
+  tags = local.common_tags
 }
