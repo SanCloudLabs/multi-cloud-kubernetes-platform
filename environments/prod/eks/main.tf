e997285647ac2bd59054bd90495b86e2c3ae8c2f
Shan <saan_sfs1@outlook.com>
2026-08-02 16:55:45 +0530

    Format Terraform configuration

--- environments/prod/eks/main.tf
+++ environments/prod/eks/main.tf
@@ -2,12 +2,12 @@ module "eks" {
 
   source = "../../../modules/eks"
 
-  instance_types =["t3.medium"]
-  desired_size = 3
-  min_size = 2
-  max_size = 4
-  cluster_name       = var.cluster_name
-  cluster_version    = var.cluster_version
+  instance_types  = ["t3.medium"]
+  desired_size    = 3
+  min_size        = 2
+  max_size        = 4
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
 
   vpc_id             = var.vpc_id
   private_subnet_ids = var.private_subnet_ids
